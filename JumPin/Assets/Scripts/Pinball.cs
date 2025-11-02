40ec7d0d92c7296414a36544da0bc9269351f6a3
JC402 <jarsenault@rollins.edu>
2025-11-02 02:10:48 -0500

    Fixed Movement
    It now properly checks if the ball is grounded, although it doesn't work on slopes.
    
    Moving left or right will accelerate the ball until a top speed is reached.
    
    The ball will also slow down fast when the player isn't pressing A or D and the ball is grounded.
    
    I also put each static object in the "Ground" layer. This is what's used by the ball to know what counts as the ground.

--- JumPin/Assets/Scripts/Pinball.cs
+++ JumPin/Assets/Scripts/Pinball.cs
@@ -2,36 +2,65 @@ using UnityEngine;
 
 public class Pinball : MonoBehaviour
 {
+    Rigidbody2D body;
+    LayerMask ground;
+    bool leftPressed, rightPressed, jumpPressed;
+
+    // Maximum distance of raycast for checking if the ball is grounded
+    public float maxRayGrndCheckDist;
+    public float maxRollSpeed; // Maximum speed the ball will roll at
+    public float jumpSpeed; // Upwards speed of ball when jump is pressed
+
+    // Ground friction coefficient when not trying to roll
+    [Range(0, 1)]
+    public float noInputFriction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
-    public Rigidbody2D body;
-    public CircleCollider2D collider;
-    public LayerMask ground;
-
-    public bool isGrounded(){
-        return Physics2D.Raycast(body.position, Vector2.down, collider.bounds.extents.y+.1f, ground);
-    }
-
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        collider = GetComponent<CircleCollider2D>();
+        ground = LayerMask.GetMask("Ground");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.A))
+            leftPressed = true;
+        else
+            leftPressed = false;
 
+        if (Input.GetKey(KeyCode.D))
+            rightPressed = true;
+        else
+            rightPressed = false;
+        
+        if (Input.GetKey(KeyCode.W))
+            jumpPressed = true;
+        else
+            jumpPressed = false;
+    }
 
-        if ((Input.GetKey(KeyCode.A))){
-            body.AddForceX(-2);
-        }
-        if ((Input.GetKey(KeyCode.D))){
-            body.AddForceX(2);
-        }
+    // Put physics update stuff here
+    void FixedUpdate()
+    {
+        if (leftPressed)
+            body.AddForceX(-maxRollSpeed - body.linearVelocity.x);
 
-        if ((Input.GetKey(KeyCode.W))&&isGrounded()) {
-            body.AddForceY(10);
+        if (rightPressed)
+            body.AddForceX(maxRollSpeed - body.linearVelocity.x);
+
+        // If ball is touching ground...
+        if (Physics2D.Raycast(transform.position, Vector2.down,
+            maxRayGrndCheckDist, ground)) {
+            // Add friction to ball if no left or right input detected
+            if (!leftPressed && !rightPressed)
+                body.AddForceX(-body.linearVelocity.x * noInputFriction,
+                               ForceMode2D.Impulse);
+
+            // Handle jump
+            if (jumpPressed)
+                body.linearVelocity = new Vector2(body.linearVelocity.x, jumpSpeed);
         }
     }
 }
